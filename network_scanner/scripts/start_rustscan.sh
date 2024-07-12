c476b38d25ef2508f07bc141d0c6b85d47dddc24
LaggerIsME <pythondelay@gmail.com>
2024-07-12 17:45:05 +0500

    Changed all to work rustscan script with outside config

--- network_scanner/scripts/start_rustscan.sh
+++ network_scanner/scripts/start_rustscan.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-rustscan --greppable --accessible --scan-order $SCAN_MODE --batch-size $BATCH_SIZE -a $PREY_IPS > /app/output.txt 2> /var/log/cron.err
+echo "[DATE_OF_SCRIPT_WORK]"
+
+rustscan --greppable --accessible --scan-order $SCAN_MODE --batch-size $BATCH_SIZE -a $PREY_IPS > /app/output.txt 2>> /var/log/cron.err
+
+echo /app/output.txt >> /var/log/cron.log
+
+echo "Scan completed. Output written to /app/output.txt. Errors written to /var/log/cron.err" >> /var/log/cron.log
--- network_scanner/scripts/startup.sh
+++ network_scanner/scripts/startup.sh
@@ -1,2 +1,6 @@
+echo "[DATE_OF_SCRIPT_START]"
+
 # Add env variables for cron
 env >> /etc/environment
+
+echo "Added env variables" >> /var/log/cron.log
